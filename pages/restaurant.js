9c2d16f462b0aaf6cd82919b56ec28f14df8104b
alias722 <wei@sivir.pw>
2021-07-29 21:37:37 +0800

    rearrange

--- pages/restaurant.js
+++ pages/restaurant.js
@@ -22,8 +22,28 @@ function Restaurant({ data }) {
             setPhotoHeight(240)
         }
     }, [width])
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+            width,
+            height
+        };
+    }
 
-    
+    function useWindowDimensions() {
+        const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+        useEffect(() => {
+            function handleResize() {
+                setWindowDimensions(getWindowDimensions());
+            }
+
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }, []);
+
+        return windowDimensions;
+    }
 
     return (
         <div id="page-wrapper">
@@ -72,29 +92,6 @@ function Restaurant({ data }) {
             <Footer />
         </div>
     )
-}
-
-function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  }
-
-function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return windowDimensions;
 }
 
 
