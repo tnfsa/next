a56fd4224132f7a68fbaa82594a4c662adf37760
alias722 <wei@sivir.pw>
2021-07-29 16:49:47 +0800

    photo resize

--- components/Section.js
+++ components/Section.js
@@ -1,22 +1,27 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import { Grid } from '@material-ui/core'
+import Image from 'next/image'
 
 export default function Section(props) {
-    console.log(props.picture)
+    console.log(props.link)
     return (
         <Grid item>
             <div className="card">
                 <div className="cardPhoto">
                     <Image src={typeof (props.picture) === "undefined" ? "https://raw.sivir.pw/public/images/pic04.jpg" : props.picture}
                         alt={`${props.title}的照片`}
-                        layout="fill" />
+                        width="100vw"
+                        height="100vw" />
                 </div>
                 <div className="container">
                     <h4>{props.title}</h4>
-                    <h4>{props.context}</h4>
+                    <h4 style={{fontSize: '1em', color: 'grey'}}>{props.context}</h4>
                     {typeof (props.link) !== "undefined" &&
-                        <Link href={props.link} >Learn More</Link>
+                        <Link href={props.link}>
+                            <a className="customLink">
+                                Learn More
+                            </a>
+                        </Link>
                     }
                 </div>
             </div>
--- pages/restaurant.js
+++ pages/restaurant.js
@@ -1,14 +1,39 @@
 import Title from '../components/Title'
 import Footer from '../components/Footer'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { Grid } from '@material-ui/core'
 
 function Restaurant({ data }) {
+    const { height, width } = useWindowDimensions()
+    const [loggedIn, setLoggedIn] = useState(true)
+    const [photoWidth, setPhotoWidth] = useState(250)
+    const [photoHeight, setPhotoHeight] = useState(150)
+    useEffect(() => {
+        if (localStorage.getItem('account_type') === null) {
+            setLoggedIn(false)
+        }
+        if (width < 736) {
+            setPhotoWidth(250)
+            setPhotoHeight(150)
+        } else {
+            setPhotoWidth(400)
+            setPhotoHeight(240)
+        }
+    }, [width])
+
     
+
     return (
         <div id="page-wrapper">
             <Title title="餐廳"
                 link="/restaurant" />
-
+            {!loggedIn &&
+                <div className="notification">
+                    <h1>點餐請先登入</h1>
+                </div>
+            }
             <section id="main">
                 <div className="container">
                     {typeof (data) === "undefined" ?
@@ -16,26 +41,31 @@ function Restaurant({ data }) {
                             <h2><center>查無資料</center></h2>
                         </article>
                         :
-                        data.map((data) => {
-                            return (
-                                <article className="box post" key={data.name}>
-                                    <div className="image featured mediaSize">
-                                        <Image src={typeof (data.picUrl) === "undefined" ? "https://database.tnfsa.org/images/pic01.jpg" : data.picUrl}
-                                            alt={`商家-${data.name}-的照片`}
-                                            layout="fill" />
-                                    </div>
-                                    <header>
-                                        <h2>{data.name}</h2>
-                                        <p>{data.description}</p>
-                                    </header>
-                                    <footer>
-                                        <ul className="actions">
-                                            <li><a href={`/order/${data.id}`} className="button">立即前往</a></li>
-                                        </ul>
-                                    </footer>
-                                </article>
-                            )
-                        })
+                        <Grid container justifyContent="center" spacing={2}>
+                            {data.map((data) => {
+                                return (
+                                    <Grid item>
+                                        <div className="custombox" key={data.name}>
+                                            <div>
+                                                <Image src={typeof (data.picUrl) === "undefined" ? "https://database.tnfsa.org/images/pic01.jpg" : data.picUrl}
+                                                    alt={`商家-${data.name}-的照片`}
+                                                    height={photoHeight}
+                                                    width={photoWidth} />
+                                            </div>
+                                            <header>
+                                                <h2>{data.name}</h2>
+                                                <p>{data.description}</p>
+                                            </header>
+                                            <footer>
+                                                <ul className="actions">
+                                                    <li><Link href={`/order/${data.id}`}><a className="button">立即查看</a></Link></li>
+                                                </ul>
+                                            </footer>
+                                        </div>
+                                    </Grid>
+                                )
+                            })}
+                        </Grid>
                     }
                 </div>
             </section>
@@ -43,6 +73,30 @@ function Restaurant({ data }) {
         </div>
     )
 }
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
+
+function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
+}
+
 
 export async function getStaticProps() {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/stores`, {
