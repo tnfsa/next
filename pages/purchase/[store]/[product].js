f5a326890b29f2c21c661f35d3b14f31b42c0d73
alias722 <wei@sivir.pw>
2021-08-07 15:05:09 +0800

    correct GA route
    	modified:   pages/purchase/[store]/[product].js

--- pages/purchase/[store]/[product].js
+++ pages/purchase/[store]/[product].js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import { useState, useEffect } from 'react'
 import Swal from 'sweetalert2'
 import Cookies from 'universal-cookie'
-import * as ga from '../components/GA'
+import * as ga from '../../../components/GA'
 
 function Purchase({ data, storeName }) {
     const [comment, setComment] = useState('')
