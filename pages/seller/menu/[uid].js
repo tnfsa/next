e2007566b07247e8505e9354c1d36f9634a80d22
milliax <wei@sivir.pw>
2021-08-12 19:42:48 +0800

    fix input but i didn't test it

--- pages/seller/menu/[uid].js
+++ pages/seller/menu/[uid].js
@@ -2,8 +2,9 @@ import Title from '../../../components/Title'
 import Authenticate from '../../../components/authenticate'
 import { useRouter } from 'next/router'
 import Cookies from "universal-cookie";
-import { Button, Col, Container, Figure, Row, Spinner } from "react-bootstrap";
+import { Button, Figure, Spinner } from "react-bootstrap";
 import { useEffect, useState } from 'react'
+import Swal from 'sweetalert2';
 
 export default function configMenu() {
     const cookies = new Cookies()
@@ -11,47 +12,48 @@ export default function configMenu() {
     const { uid } = router.query
 
     const [name, setName] = useState('')
-    const [subtitle, setSubtitle] = useState('')
+    const [description, setDescription] = useState('')
     const [price, setPrice] = useState('')
     const [image, setImage] = useState('')
     const [uploading, setUploading] = useState(false)
     const [imageUrl, setImageUrl] = useState('')
+
     const storeId = cookies.get('store_id')
     const session = cookies.get('session')
     async function Send() {
-        const postURL = process.env.NEXT_PUBLIC_API_ENDPOINT + '/stores/' + storeId + '/products'
+        const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/stores/${storeId}/products`
 
-        const data = {
-            'name': name,
-            'price': price,
-            'description': subtitle,
-            'store_id': storeId,
-            "image": imageUrl
-        }
-        //window.alert(postURL)
-        await fetch(postURL, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-                "Accept": "application/json",
-                "Authorization": `Bearer ${session}`
-            }
-        }).then(response => {
-            if (response.ok) {
-                return response.json()
-            }
-            return response.text().then(res => {
-                throw new Error(res)
+        try {
+            const res = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Accept": "application/json",
+                    "Authorization": `Bearer ${session}`
+                },
+                body: JSON.stringify({
+                    name,
+                    price,
+                    description,
+                    'store_id': storeId,
+                    "image": imageUrl
+                })
             })
-        }).catch((error) => {
-            console.log(error.message)
-            let response = JSON.parse(error.message)
-            window.alert(`${response.message}\n與伺服器連線錯誤，請再試一次\n如果問題無法解決，請聯絡管理員`)
-        }).then(response => {
+            const response = await res.json()
             console.log(response)
-            history.push('/config/menu')
-        })
+            await Swal.fire({
+                icon: 'success',
+                title: '上傳成功'
+            })
+        } catch (err) {
+            await Swal.fire({
+                icon: 'error',
+                title: '上傳錯誤',
+                text: err
+            })
+
+        }
+        await router.push('/seller/menu')
     }
 
     function handleChangeImage(evt) {
@@ -65,32 +67,28 @@ export default function configMenu() {
             const formData = new FormData()
             formData.append('image', file)
             setUploading(true)
-            fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + '/stores/' + storeId + '/images', {
-                method: 'POST',
-                headers: {
-                    "Accept": "application/json",
-                    "Authorization": `Bearer ${session}`
-                },
-                body: formData
-            }).then(response => {
-                if (response.ok) {
-                    return response.json()
-                }
-                return response.text().then(res => {
-                    throw new Error(res)
+            try {
+                const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + '/stores/' + storeId + '/images', {
+                    method: 'POST',
+                    headers: {
+                        "Accept": "application/json",
+                        "Authorization": `Bearer ${session}`
+                    },
+                    body: formData
                 })
-            }).catch((error) => {
-                console.log(error.message)
-                let response = JSON.parse(error.message)
-                window.alert(`${response.message}\n與伺服器連線錯誤，請再試一次\n如果問題無法解決，請聯絡管理員`)
-            }).then(response => {
-                console.log(typeof response)
-                //window.alert(response)
+                const response = await res.json()
+                console.log(response)
                 setImageUrl(response.result.data)
-                window.alert(response.result.data)
-                setUploading(false)
-            })
-        };
+
+            } catch (err) {
+                await Swal.fire({
+                    icon: 'error',
+                    title: '上傳錯誤',
+                    text: err
+                })
+            }
+            setUploading(false)
+        }
         reader.readAsDataURL(file);
         console.log("Uploaded");
     }
@@ -106,7 +104,7 @@ export default function configMenu() {
                     'Accept': 'application/json'
                 }
             })
-            let parsed = await data.json()
+            const parsed = await data.json()
             setPrice(parsed['price'])
             setName(parsed['name'])
             setSubtitle(parsed['description'])
@@ -114,12 +112,12 @@ export default function configMenu() {
             setImageUrl(parsed['image'])
             console.log(parsed)
         } catch (err) {
-            window.alert(
-                `錯誤!!!
-error: ${err}
-立即返回首頁
-`)
-            document.location.href = '/'
+            await Swal.fire({
+                icon: 'error',
+                title: '伺服器讀取錯誤',
+                text: err
+            })
+            await router.push('/')
         }
     }
     useEffect(() => {
@@ -142,75 +140,75 @@ error: ${err}
                             Send()
                         }}
                     >
-                        <div className="form-group">
-                            <label>食物名</label>
-                            <input type="text"
-                                className="form-control"
-                                placeholder="標題"
+                        <div class="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                食物名
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="text"
+                                placeholder="請輸入"
                                 value={name}
-                                onChange={e => setName(e.target.value)}
+                                onChange={event => { setName(event.target.value) }}
                                 required />
                         </div>
-                        <div className="form-group">
-                            <label>價錢</label>
-                            <input type="text"
-                                className="form-control"
-                                placeholder="價錢"
+                        <div class="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                價錢
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="text"
+                                placeholder="請輸入"
                                 value={price}
-                                onChange={e => setPrice(parseInt(e.target.value))}
+                                onChange={event => { setPrice(parseInt(event.target.value)) }}
                                 required />
                         </div>
-                        <div className="form-group">
-                            <label>副標題</label>
-                            <input type="text"
-                                className="form-control"
-                                placeholder="副標題"
-                                value={subtitle}
-                                onChange={e => setSubtitle(e.target.value)}
+                        <div class="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                副標題
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="text"
+                                placeholder="請輸入"
+                                value={description}
+                                onChange={event => { setDescription(event.target.value) }}
                                 required />
                         </div>
-                        <Row>
-                            <Col>
-                                <div className="form-group">
-                                    <Button as={"label"} variant={"primary"} hidden={imageUrl !== ''}>
-                                        上傳圖片
-                                        <input type="file" name="file"
-                                            className="upload-file"
-                                            id="file"
-                                            onChange={handleChangeImage}
-                                            hidden
-                                        />
-                                    </Button>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <center>
-                                    <Spinner hidden={uploading === false} animation={"border"} />
-                                    <br />
-                                    <a href={process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl} target="_blank"
-                                        hidden={!image || uploading}
-                                        rel="noreferrer">{process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl}</a>
-                                    <br />
-                                    <Figure.Image
-                                        width={300}
-                                        src={image && !uploading ? process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl : "https://via.placeholder.com/300x180?text=Product+Image"}
-                                        resizeMode="contain"
+                        <div className="justify-center">
+                            <div className="form-group">
+                                <Button as={"label"} variant={"primary"} hidden={imageUrl !== ''}>
+                                    上傳圖片
+                                    <input type="file" name="file"
+                                        className="upload-file"
+                                        id="file"
+                                        onChange={handleChangeImage}
+                                        hidden
                                     />
-                                </center>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <div className="h-100 align-items-center">
-                                <div id="placeToAdd" />
+                                </Button>
                             </div>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <button className="btn btn-primary btn-block">送出</button>
-                            </Col>
-                        </Row>
+                        </div>
+                        <div className="justify-center">
+                            {uploading &&
+                                <Spinner animation={"border"} />
+                            }
+                            <br />
+                            <a href={process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl} target="_blank"
+                                hidden={!image || uploading}
+                                rel="noreferrer">{process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl}</a>
+                            <br />
+                            <Figure.Image
+                                width={300}
+                                src={image && !uploading ? process.env.NEXT_PUBLIC_API_HOST + imageUrl : "https://via.placeholder.com/300x180?text=Product+Image"}
+                                resizeMode="contain"
+                            />
+                        </div>
+
+                        <div className="h-100 justify-center">
+                            <div id="placeToAdd" />
+                        </div>
+
+                        <div className="justify-center">
+                            <button className="btn btn-primary btn-block">送出</button>
+                        </div>
                     </form>
                 </div>
             </section>
--- pages/seller/menu/new.js
+++ pages/seller/menu/new.js
@@ -1,20 +1,25 @@
 import Title from '../../../components/Title'
 import Authenticate from '../../../components/authenticate'
 import Cookies from 'universal-cookie'
-import { useState} from 'react'
-import {useRouter} from 'next/router'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 //deprecated
-import {Button, Col, Container, Figure, Row, Spinner} from "react-bootstrap";
+import { Button, Figure, Spinner } from "react-bootstrap";
+import Swal from 'sweetalert2'
 
 export default function NewMenu() {
     const [image, setImage] = useState('')
     const [uploading, setUploading] = useState(false)
     const [imageUrl, setImageUrl] = useState('')
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+    const [description, setDescription] = useState('')
     const cookies = new Cookies()
     const storeId = cookies.get('store_id')
     const session = cookies.get('session')
     const router = useRouter()
+
     const NewOption = () => {
         const popupUrl = config['project'] + '#/config/new/option'
         const popupWindow = window.open(popupUrl,/*'popUpWindow',*/'', 'location=no,height=500,width=400,top=100,left=300')
@@ -22,43 +27,39 @@ export default function NewMenu() {
     }
 
     async function Send() {
-        const name = document.getElementById('foodTitle').value
-        const subtitle = document.getElementById('foodSub').value
-        const price = document.getElementById('foodPrice').value
-        const postURL = process.env.NEXT_PUBLIC_API_ENDPOINT + '/stores/' + storeId + '/products'
+        const url = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/stores/${storeId}/products`
 
-        var data = {
-            name,
-            price,
-            'description': subtitle,
-            'store_id': storeId,
-            "image": imageUrl
-        }
-        //window.alert(postURL)
-        await fetch(postURL, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-                "Accept": "application/json",
-                "Authorization": `Bearer ${session}`
-            }
-        }).then(response => {
-            if (response.ok) {
-                return response.json()
-            }
-            return response.text().then(res => {
-                throw new Error(res)
+        try {
+            const res = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Accept": "application/json",
+                    "Authorization": `Bearer ${session}`
+                },
+                body: JSON.stringify({
+                    name,
+                    price,
+                    description,
+                    'store_id': storeId,
+                    "image": imageUrl
+                })
             })
-        }).catch((error) => {
-            console.log(error.message)
-            let response = JSON.parse(error.message)
-            window.alert(`${response.message}\n與伺服器連線錯誤，請再試一次\n如果問題無法解決，請聯絡管理員`)
-        }).then(response => {
+            const response = await res.json()
             console.log(response)
-            router.push()
-            history.push('/config/menu')
-        })
+            await Swal.fire({
+                icon: 'success',
+                title: '上傳成功'
+            })
+        } catch (err) {
+            await Swal.fire({
+                icon: 'error',
+                title: '上傳錯誤',
+                text: err
+            })
+
+        }
+        await router.push('/seller/menu')
     }
 
     function handleChangeImage(evt) {
@@ -72,31 +73,28 @@ export default function NewMenu() {
             const formData = new FormData()
             formData.append('image', file)
             setUploading(true)
-            fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + '/stores/' + storeId + '/images', {
-                method: 'POST',
-                headers: {
-                    "Accept": "application/json",
-                    "Authorization": `Bearer ${session}`
-                },
-                body: formData
-            }).then(response => {
-                if (response.ok) {
-                    return response.json()
-                }
-                return response.text().then(res => {
-                    throw new Error(res)
+            try {
+                const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + '/stores/' + storeId + '/images', {
+                    method: 'POST',
+                    headers: {
+                        "Accept": "application/json",
+                        "Authorization": `Bearer ${session}`
+                    },
+                    body: formData
                 })
-            }).catch((error) => {
-                console.log(error.message)
-                let response = JSON.parse(error.message)
-                window.alert(`${response.message}\n與伺服器連線錯誤，請再試一次\n如果問題無法解決，請聯絡管理員`)
-            }).then(response => {
-                console.log(typeof response)
-                //window.alert(response)
+                const response = await res.json()
+                console.log(response)
                 setImageUrl(response.result.data)
-                setUploading(false)
-            })
-        };
+
+            } catch (err) {
+                await Swal.fire({
+                    icon: 'error',
+                    title: '上傳錯誤',
+                    text: err
+                })
+            }
+            setUploading(false)
+        }
         reader.readAsDataURL(file);
         console.log("Uploaded");
     }
@@ -116,66 +114,78 @@ export default function NewMenu() {
                     </div>
                     <form
                         className={"foodBlock"}
-                        onSubmit={e => {
-                            e.preventDefault()
+                        onSubmit={event => {
+                            event.preventDefault()
                             Send()
-                        }}
-                    >
-                        <div className="form-group">
-                            <label>食物名</label>
-                            <input id="foodTitle" type="text" className="form-control" placeholder="標題" required />
+                        }}>
+                        <div class="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                食物名
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="text"
+                                placeholder="請輸入"
+                                value={name}
+                                onChange={event => { setName(event.target.value) }}
+                                required />
                         </div>
-                        <div className="form-group">
-                            <label>價錢</label>
-                            <input id="foodPrice" type="number" className="form-control" placeholder="價錢" required />
+                        <div class="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                價錢
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="text"
+                                placeholder="請輸入"
+                                value={price}
+                                onChange={event => { setPrice(parseInt(event.target.value)) }}
+                                required />
                         </div>
-                        <div className="form-group">
-                            <label>副標題</label>
-                            <input id="foodSub" type="text" className="form-control" placeholder="副標" required />
+                        <div class="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                副標題
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                type="text"
+                                placeholder="請輸入"
+                                value={description}
+                                onChange={event => { setDescription(event.target.value) }}
+                                required />
                         </div>
-                        <Row>
-                            <Col>
-                                <div className="form-group">
-                                    <Button as={"label"} variant={"primary"} hidden={imageUrl !== ''}>
-                                        上傳圖片
-                                        <input type="file" name="file"
-                                            className="upload-file"
-                                            id="file"
-                                            onChange={handleChangeImage}
-                                            required
-                                            hidden
-                                        />
-                                    </Button>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <center>
-                                    <Spinner hidden={uploading === false} animation={"border"} />
-                                    <br />
-                                    <a href={process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl} target="_blank"
-                                        hidden={!image || uploading}
-                                        rel="noreferrer">{process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl}</a>
-                                    <br />
-                                    <Figure.Image
-                                        width={300}
-                                        src={image && !uploading ? process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl : "https://via.placeholder.com/300x180?text=Product+Image"}
-                                        resizeMode="contain"
+                        <div className="justify-center">
+                            <div className="form-group">
+                                <Button as={"label"} variant={"primary"} hidden={imageUrl !== ''}>
+                                    上傳圖片
+                                    <input type="file" name="file"
+                                        className="upload-file"
+                                        id="file"
+                                        onChange={handleChangeImage}
+                                        required
+                                        hidden
                                     />
-                                </center>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <div className="h-100 align-items-center">
-                                <div id="placeToAdd" />
+                                </Button>
                             </div>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <button className="btn btn-primary btn-block">送出</button>
-                            </Col>
-                        </Row>
+                        </div>
+                        <div className="justify-center">
+                            <Spinner hidden={uploading === false} animation={"border"} />
+                            <br />
+                            <a href={process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl} target="_blank"
+                                hidden={!image || uploading}
+                                rel="noreferrer">{process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl}</a>
+                            <br />
+                            <Figure.Image
+                                width={300}
+                                src={image && !uploading ? process.env.NEXT_PUBLIC_API_HOST + '/' + imageUrl : "https://via.placeholder.com/300x180?text=Product+Image"}
+                                resizeMode="contain"
+                            />
+                        </div>
+
+                        <div className="h-100 justify-center">
+                            <div id="placeToAdd" />
+                        </div>
+
+                        <div className="justify-center">
+                            <button className="btn btn-primary btn-block">送出</button>
+                        </div>
                     </form>
                 </div>
             </section>
